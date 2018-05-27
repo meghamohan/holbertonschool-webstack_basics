1ff10d6f312b353f137c64cdedb249d8a1425926
meghamohan <meghamohan1989@gmail.com>
2018-05-27 22:22:58 +0000

    removing function

--- 0x01-python_basics/5-args.py
+++ 0x01-python_basics/5-args.py
@@ -2,22 +2,17 @@
 """
 prints all the arguments
 """
-import sys
+from  sys import argv
 
 
-def args(a):
-    """
-    prints number and the arg
-    """
-    l = len(a)
+if __name__ == "__main__":
+    l = len(argv)
     if l is 1:
         print("{} arguments.".format(l - 1))
     elif l is 2:
         print("{} argument.".format(l - 1))
-        print("{}: {}".format(l - 1, a[1]))
+        print("{}: {}".format(l - 1, argv[1]))
     else:
         print("{} arguemnts:".format(l - 1))
         for i in range(1, l):
-            print("{}: {}".format(i, a[i]))
-if __name__ == "__main__":
-    args(sys.argv)
+            print("{}: {}".format(i, argv[i]))
