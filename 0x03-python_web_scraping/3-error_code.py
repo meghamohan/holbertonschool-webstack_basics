7bfbfb287a186229390e8b2f65ccd64f82218985
meghamohan <meghamohan1989@gmail.com>
2018-06-15 11:57:45 +0000

    fixed issue with open brace on line 13

--- 0x03-python_web_scraping/3-error_code.py
+++ 0x03-python_web_scraping/3-error_code.py
@@ -3,13 +3,13 @@
 takes in a URL, sends a request to the URL
 and displays the body of the response
 """
-from requests import get
+import requests
 import sys
 
 
 if __name__ == "__main__":
-    req = get(sys.argv[1])
-    if req.status_code >= 400:
-        print('Error code: {}'.format(req.status_code)
+    req = requests.get(sys.argv[1])
+    if (req.status_code >= 400):
+        print('Error code: {}'.format(req.status_code))
     else:
         print(req.text)
