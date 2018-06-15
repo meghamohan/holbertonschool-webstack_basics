809713f95731fed4d64621e0b7bb9ea5dde2d1a7
meghamohan <meghamohan1989@gmail.com>
2018-06-15 11:17:44 +0000

    pep8 fixes

--- 0x03-python_web_scraping/0-hbtn_status.py
+++ 0x03-python_web_scraping/0-hbtn_status.py
@@ -9,5 +9,5 @@ script that fetches https://intranet.hbtn.io/status
 if __name__ == "__main__":
     req = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t- type:{}".format(type(req.text)))
-    print("\t- content:".format(req.text))
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
--- 0x03-python_web_scraping/3-error_code.py
+++ 0x03-python_web_scraping/3-error_code.py
@@ -10,6 +10,6 @@ import sys
 if __name__ == "__main__":
     req = get(sys.argv[1])
     if req.status_code >= 400:
-        print('Error code:{}'.format(req.status_code)
+        print('Error code: {}'.format(req.status_code)
     else:
         print(req.text)
