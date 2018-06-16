118eb8adeda60a44bed91fd120d1eda8cf467440
meghamohan <meghamohan1989@gmail.com>
2018-06-16 06:40:10 +0000

    adding task4

---
+++ 0x03-python_web_scraping/4-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+takes in a letter and sends a POST request
+with the letter as a parameter
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+    req = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
+    try:
+        res = req.json()
+        print(res)
+    except:
+        print("Not a valid JSON")
+    if res:
+        print("[{}] {}".format(res.get('id'), res.get('name')))
+    else:
+         print("No result")
