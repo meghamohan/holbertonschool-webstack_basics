5a2e16ee528b0249edba896366da51de8aa165dc
meghamohan <meghamohan1989@gmail.com>
2018-06-18 03:41:14 +0000

    silly mstake

--- 0x02-javascript_basics/102-concat.js
+++ 0x02-javascript_basics/102-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const a = process.argv.slice(2);
 const fs = require('fs');
-fs.readFile(args[0], 'utf8', function (err, data) {
+fs.readFile(a[0], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
     process.exit(1);
@@ -9,7 +9,7 @@ fs.readFile(args[0], 'utf8', function (err, data) {
 
 
   const d1 = data;
-  fs.readFile(args[1], 'utf8', function (err, data) {
+  fs.readFile(a[1], 'utf8', function (err, data) {
     if (err) {
       console.log(err);
       process.exit(1);
@@ -17,7 +17,7 @@ fs.readFile(args[0], 'utf8', function (err, data) {
 
 
     const d2 = data;
-    fs.writeFile(args[2], d1 + d2, function (err) {
+    fs.writeFile(a[2], d1 + d2, function (err) {
       if (err) {
         console.log(err);
         process.exit(1);
