56bc9fd2eb52f2bbfb68dd3ae3c7f24c3c630207
meghamohan <meghamohan1989@gmail.com>
2018-06-18 03:01:54 +0000

    semistd fix

--- 0x02-javascript_basics/8-rectangle.js
+++ 0x02-javascript_basics/8-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-exports.Rectangle = function Rectangle(w, h) {
+exports.Rectangle = function Rectangle (w, h) {
   if (w >= 1 && h >= 1) {
     this.width = w;
     this.height = h;
