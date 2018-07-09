862ca68f4ce8fe2732a1bf5344cd9f732b7d33af
meghamohan <meghamohan1989@gmail.com>
2018-07-09 06:43:46 +0000

    adding adv task 6

---
+++ 0x05-sql_create_join/106-not_my_genres.sql
@@ -0,0 +1,12 @@
+-- list all genres not linked with dexter
+SELECT DISTINCT tv_genres.name FROM tv_genres
+WHERE tv_genres.name NOT IN (
+    SELECT DISTINCT tv_genres.name
+    FROM tv_genres
+    INNER JOIN tv_show_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+    LEFT OUTER JOIN tv_shows
+    ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY tv_genres.name ASC;
