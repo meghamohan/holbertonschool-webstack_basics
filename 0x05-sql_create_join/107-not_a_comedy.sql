2ef87caa0439c0ab91209cf9985a6e1e051f7aa6
meghamohan <meghamohan1989@gmail.com>
2018-07-09 06:46:44 +0000

    adding adv task7

---
+++ 0x05-sql_create_join/107-not_a_comedy.sql
@@ -0,0 +1,7 @@
+-- list all shows without genre comedy
+SELECT DISTINCT tv_shows.title FROM tv_shows
+LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT OUTER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name != 'Comedy' OR tv_genres.name
+IS NULL ORDER BY tv_shows.title ASC;
